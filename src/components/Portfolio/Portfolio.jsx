5fe0220f44bcbfc3565b4dacb741b998172dff1f
Carlitossaul <carlitossaul.h@gmail.com>
2023-05-01 01:54:41 -0300

    s

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,18 +1,20 @@
 import style from "./Portfolio.module.css";
 import appRecipes from "../../assets/app-recipes.png";
 import { Link } from "react-router-dom";
+import { BsBoxArrowUpRight } from "react-icons/bs";
+import { GoMarkGithub } from "react-icons/go";
 
 const Portfolio = () => {
   return (
     <div className={style.Portfolio}>
       <div className={style.container}>
-        <div className={style.title}>
+        <div className={style.titlePag}>
           <h1 className={style.h1}> Portfolio </h1>
         </div>
         <div className={style.website}>
-          <h1 className={style.titleWeb}>Website about recipes and diets</h1>
+          <h1 className={style.titleWeb}>Are You Hungry?</h1>
           <div className={style.contentModelOne}>
-            <Link>
+            <Link to={"https://areyouhungry.vercel.app/"}>
               <img
                 className={style.image}
                 src={appRecipes}
@@ -20,6 +22,9 @@ const Portfolio = () => {
               />
             </Link>
             <div className={style.divParrafo}>
+              <h3 className={style.subtitle}>
+                Website about recipes and diets
+              </h3>
               <p className={style.parrafo}>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam
                 nesciunt minus officia earum labore nisi. Delectus reprehenderit
@@ -27,9 +32,28 @@ const Portfolio = () => {
                 Corrupti quibusdam explicabo saepe, inventore qui libero.
               </p>
               <div className={style.links}>
-                <Link>Hola</Link>
-                <Link>Hola</Link>
-                <Link>Hola</Link>
+                <Link
+                  className={style.link}
+                  to={"https://areyouhungry.vercel.app"}
+                >
+                  <BsBoxArrowUpRight className={style.icon} />
+                  <p>Visit the live site</p>
+                </Link>
+                <Link
+                  className={style.link}
+                  to={"https://github.com/Carlitossaul/PI-Food-Front"}
+                >
+                  <GoMarkGithub className={style.icon} />
+
+                  <p>View the source code on GitHub - client</p>
+                </Link>
+                <Link
+                  className={style.link}
+                  to={"https://github.com/Carlitossaul/PI-Food-back-"}
+                >
+                  <GoMarkGithub className={style.icon} />
+                  <p>View the source code on GitHub - api</p>
+                </Link>
               </div>
             </div>
           </div>
